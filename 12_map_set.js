2778b5aeda279712d7b22e3ad2bb5040c6dcd3d4
saniok333 <alex.o.csc@gmail.com>
2020-03-02 13:43:59 +0200

    map and set

--- 12_map_set.js
+++ 12_map_set.js
@@ -4,8 +4,26 @@ const map = new Map(
     ]
 )
 
-console.log(map.get('a'))
+// console.log(map.get('a'))
 map.set('b', 2).set(NaN, 'Number').set(42, 'demo')
-console.log(map.get(NaN))
+// console.log(map.get(NaN))
 // map.clear()
-console.log(map.has(42))
+// console.log(map.has(42))
+// map.delete('b')
+// console.log(map.size)
+// console.log(map.keys())
+// console.log(map.entries())
+// console.log(map.values())
+
+//set
+
+const set = new Set([1, 1, 2, 3, 5, 8, 13, 13, 13, 5, 8])
+// console.log(set.size)
+console.log(set.add(21))
+// set.clear()
+// set.delete(3)
+// console.log(set)
+
+console.log(set.keys())
+console.log(set.entries())
+console.log(set.values())
